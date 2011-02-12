62a63ea75af297ae27d9820e621d5083027e7825
aljen <harakash@gmail.com>
2011-02-12 21:37:35 +0100

    Removed useless comments.

--- include/kernel/isr.h
+++ include/kernel/isr.h
@@ -43,11 +43,11 @@ typedef struct registers_t {
   uint32_t esi;
   uint32_t ebp;
   uint32_t esp;
-  uint32_t ebx; // ;
-  uint32_t edx; // ;
-  uint32_t ecx; // ;
-  uint32_t eax; // ;
-  uint32_t int_no; // ;
+  uint32_t ebx;
+  uint32_t edx;
+  uint32_t ecx;
+  uint32_t eax;
+  uint32_t int_no;
   uint32_t err_code;
   uint32_t eip;
   uint32_t cs;
