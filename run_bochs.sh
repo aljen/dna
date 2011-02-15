ba110578d220a4b9216b8b1dcdd115c6c2cf4071
aljen <harakash@gmail.com>
2011-02-15 09:19:05 +0100

    Updated how to boot information.

--- run_bochs.sh
+++ run_bochs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:"/c/Program Files (x86)/Bochs-2.4.5/"
 echo "Type in bochs:"
-echo "kernel 200+30"
+echo "kernel 200+100"
 echo "boot"
+echo > image/system.log
 bochs -q -f image/bochsrc.bxrc
