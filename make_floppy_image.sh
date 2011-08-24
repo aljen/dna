d6b18a8b117fc5271103f2c767b412970e4aa7f5
aljen <harakash@gmail.com>
2011-08-24 06:45:21 +0200

    Removed more unused stuff.

--- make_floppy_image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat image/stage1 image/stage2 image/pad kernel/dist/Debug/CrossTools-Windows/kernel > image/floppy.img
--- run_bochs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:"/c/Program Files (x86)/Bochs-2.4.5/"
-echo "Type in bochs:"
-echo "kernel 200+100"
-echo "boot"
-echo > image/system.log
-bochs -q -f image/bochsrc.bxrc
