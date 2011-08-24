0b141606c9627b46e5d0f6cdb5f60debd13f96b3
aljen <harakash@gmail.com>
2011-08-24 07:54:54 +0200

    Updated vhd scripts to format & mount as ext2 on win7 (using ext2fsd project).

--- scripts/vhd-create.bat
+++ scripts/vhd-create.bat
@@ -1,15 +1,22 @@
 @echo off
 set DISKNAME=disk
 set DISKSIZE=128
+set DISKLETTER=z
 if not "%1"=="" set DISKNAME=%1
 if not "%2"=="" set DISKSIZE=%2
-echo Creating %CD%\%DISKNAME%.vhd with size %DISKSIZE% MB
+if not "%3"=="" set DISKLETTER=%3
+echo Creating %CD%\%DISKNAME%.vhd with size %DISKSIZE% MB (ext2)
 echo create vdisk file="%CD%\%DISKNAME%.vhd" maximum=%DISKSIZE% type=fixed noerr > script.vhd
 echo select vdisk file="%CD%\%DISKNAME%.vhd" >> script.vhd
-echo attach vdisk >> script.vhd
+echo attach vdisk noerr>> script.vhd
 echo clean >> script.vhd
-echo create partition primary >> script.vhd
-echo format fs=fat32 label="DNA" quick >> script.vhd
+echo create partition primary noerr>> script.vhd
+echo select partition 1 >> script.vhd
+echo active >> script.vhd
+echo assign letter=%DISKLETTER% >> script.vhd
+diskpart /s script.vhd > nul
+"c:\program files\ext2fsd\mke2fs" -b 4096 -L DNA %DISKLETTER%: < nul > nul
+echo select vdisk file="%CD%\%DISKNAME%.vhd" > script.vhd
 echo detach vdisk >> script.vhd
 diskpart /s script.vhd > nul
 del script.vhd
--- scripts/vhd-mount.bat
+++ scripts/vhd-mount.bat
@@ -7,8 +7,6 @@ if not exist %CD%\%DISKNAME%.vhd goto NOTFOUND
 echo Mounting %CD%\%DISKNAME%.vhd as %DISKLETTER%:
 echo select vdisk file="%CD%\%DISKNAME%.vhd" > script.vhd
 echo attach vdisk >> script.vhd
-echo select partition 1 >> script.vhd
-echo assign letter=%DISKLETTER% >> script.vhd
 diskpart /s script.vhd > nul
 del script.vhd
 goto END
