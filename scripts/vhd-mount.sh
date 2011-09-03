ce847bd10b7418b0686c5bed56c3a616c10a5975
aljen <harakash@gmail.com>
2011-09-03 21:39:08 +0200

    Removed no longer needed vhd-* shell scripts.

--- scripts/vhd-mount.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $1 - disk name
-# $2 - mnt point
-losetup /dev/loop0 -o 512 $1
-sleep 1
-fuseext2 -o rw+ /dev/loop0 $2
--- scripts/vhd-umount.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $1 - mnt point
-fusermount -u $1
-sleep 1
-losetup -d /dev/loop0
-
