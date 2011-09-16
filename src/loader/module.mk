4f2acede2289a58ec7553c6820670151bd9868e5
aljen <aljen@hitomi.pl>
2011-09-16 08:16:58 +0200

    Fixed loader module.mk.

--- src/loader/module.mk
+++ src/loader/module.mk
@@ -10,10 +10,10 @@ $(eval $(call add-plain-bin,stage2.bin,$(SOURCES)))
 
 $(eval $(call create-directories))
 
-install-loader: create umount $(BUILD)/tools/writebootloader \
-$(BUILD)/loader/stage1.bin $(BUILD)/loader/stage2.bin
+install-loader: create umount $(TARGETS)/tools/writebootloader \
+  $(TARGETS)/loader/stage1.bin $(TARGETS)/loader/stage2.bin
 	@echo "$(MSG_BOOTLOADER) $(DISKNAME) $(CRED)<- $(CNONE)stage{1,2}.bin"
-	$(V)$(BUILD)/tools/writebootloader $(DISKNAME) $(BUILD)/loader/stage1.bin \
-		$(BUILD)/loader/stage2.bin 2> /dev/null
+	$(V)$(TARGETS)/tools/writebootloader $(DISKNAME) $(TARGETS)/loader/stage1.bin\
+		$(TARGETS)/loader/stage2.bin 2> /dev/null
 
 install: umount install-loader mount
